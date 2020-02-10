2fdf39866d5f0e474d51e3a315b95d96e240133b
Roumiana Bourkova <48831445+rumywanderung@users.noreply.github.com>
2020-02-10 12:07:56 +0100

    working on score/highscore comparison

--- Runner/Assets/Scripts/GameManager.cs
+++ Runner/Assets/Scripts/GameManager.cs
@@ -21,67 +21,78 @@ public class GameManager : MonoBehaviour
     public GameObject screen;
 
     public Text score;
-    public int Points = 0;
+    public Text highscore = null;
+    public int Points;
     public int HighPoints = 0;
-    public GameObject highscore = null;
+    
 
-    public bool Overed = false;
-
-    int i = 3;
+    public bool Overed;
 
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
-        DontDestroyOnLoad(score);
-        DontDestroyOnLoad(score.gameObject);
     }
 
     void Start()
     {
-        heart = Instantiate((Resources.Load("HEARTOBJECT")), new Vector2(7.98F, 5.88F), Quaternion.identity) as GameObject;
-        heart2 = Instantiate((Resources.Load("HEARTOBJECT")), new Vector2(8.62F, 5.88F), Quaternion.identity) as GameObject;
-        heart3 = Instantiate((Resources.Load("HEARTOBJECT")), new Vector2(9.27F, 5.88F), Quaternion.identity) as GameObject;
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
 
-        fruit1 = Instantiate((Resources.Load("fruit1")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
-        fruit2 = Instantiate((Resources.Load("fruit2")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
-        fruit3 = Instantiate((Resources.Load("fruit3")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
-        fruit4 = Instantiate((Resources.Load("fruit4")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
-        fruit5 = Instantiate((Resources.Load("fruit5")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
-        fruit6 = Instantiate((Resources.Load("fruit6")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+            Points = 0;
+            Overed = false;
 
-        fruit1.transform.SetParent(Tiles);
-        fruit2.transform.SetParent(Tiles);
-        fruit3.transform.SetParent(Tiles);
-        fruit4.transform.SetParent(Tiles);
-        fruit5.transform.SetParent(Tiles);
-        fruit6.transform.SetParent(Tiles);
+            heart = Instantiate((Resources.Load("HEARTOBJECT")), new Vector2(7.98F, 5.88F), Quaternion.identity) as GameObject;
+            heart2 = Instantiate((Resources.Load("HEARTOBJECT")), new Vector2(8.62F, 5.88F), Quaternion.identity) as GameObject;
+            heart3 = Instantiate((Resources.Load("HEARTOBJECT")), new Vector2(9.27F, 5.88F), Quaternion.identity) as GameObject;
+
+            fruit1 = Instantiate((Resources.Load("fruit1")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+            fruit2 = Instantiate((Resources.Load("fruit2")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+            fruit3 = Instantiate((Resources.Load("fruit3")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+            fruit4 = Instantiate((Resources.Load("fruit4")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+            fruit5 = Instantiate((Resources.Load("fruit5")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+            fruit6 = Instantiate((Resources.Load("fruit6")), new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F)), Quaternion.identity) as GameObject;
+
+            fruit1.transform.SetParent(Tiles);
+            fruit2.transform.SetParent(Tiles);
+            fruit3.transform.SetParent(Tiles);
+            fruit4.transform.SetParent(Tiles);
+            fruit5.transform.SetParent(Tiles);
+            fruit6.transform.SetParent(Tiles);
+        }
     }
 
     void Update()
     {
         if (SceneManager.GetActiveScene().name == "GameOver")
         {
+            Transform GOScore;
+            Transform GOHighscore;
+
             if (Overed == false)
             {
                 Debug.Log("gameover");
-                highscore = Instantiate((Resources.Load("highscore") as GameObject), new Vector2(-194.1F, -23.8F), Quaternion.identity);
+                //highscore = Instantiate((Resources.Load("highscore") as GameObject), new Vector2(-194.1F, -23.8F), Quaternion.identity);
                 Canvas canva = FindObjectOfType<Canvas>();
-                highscore.transform.SetParent(canva.transform);
-                score.transform.SetParent(canva.transform);
+                GOScore = canva.transform.GetChild(1);
+                GOHighscore = canva.transform.GetChild(2);
+                //highscore.transform.SetParent(canva.transform);
+                //score.transform.SetParent(canva.transform);
 
                 if (Points > HighPoints)
                 {
-                    score.text = "YOUR SCORE: " + Points.ToString();
-                    highscore.GetComponent<Text>().text = "BEST SCORE: " + Points.ToString();
+                    GOScore.GetComponent<Text>().text = "YOUR SCORE: " + Points.ToString();
+                    GOHighscore.GetComponent<Text>().text = "BEST: " + Points.ToString();
+                    HighPoints = Points;
                     return;
                 }
                 else if (Points < HighPoints)
                 {
-                    score.text = "YOUR SCORE: " + Points.ToString();
+                    GOScore.GetComponent<Text>().text = "YOUR SCORE: " + Points.ToString();
                     return;
                 }
 
                 Overed = true;
+                DontDestroyOnLoad(GOHighscore.gameObject);
             }
         }
     }
--- Runner/Assets/Scripts/Player.cs
+++ Runner/Assets/Scripts/Player.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     GameManager manager;
+    Canvas canva;
+    Transform ScoreText;
 
-    int i = 2;
+    int i;
 
     void Start()
     {
         manager = FindObjectOfType<GameManager>();
-
+        canva = FindObjectOfType<Canvas>();
+        ScoreText = canva.transform.GetChild(1);
+        i = 2;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -48,7 +53,7 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "fruit")
         {
             manager.Points += 1;
-            manager.score.text = "SCORE: " + manager.Points.ToString();
+            ScoreText.GetComponent<Text>().text = "SCORE: " + manager.Points.ToString();
             collision.gameObject.transform.position = new Vector2(Random.Range(16, 100), Random.Range(0.22F, 2F));
         }
 
